237d3733cad5c931bfc895f1cacb307261a9cf57
Paphop Sawasdee <paphop_sawasdee@earthbrain.com>
2025-04-03 16:36:44 +0900

    Use prepare for action payload

--- src/features/posts/AddPostForm.tsx
+++ src/features/posts/AddPostForm.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { nanoid } from '@reduxjs/toolkit'
 
 import { useAppDispatch } from '@/app/hooks'
 
-import { type Post, postAdded } from './postsSlice'
+import { postAdded } from './postsSlice'
 
 interface AddPostFormFields extends HTMLFormControlsCollection {
   postTitle: HTMLInputElement
@@ -22,12 +21,7 @@ export const AddPostForm = () => {
     const title = elements.postTitle.value
     const content = elements.postContent.value
 
-    const newPost: Post = {
-      id: nanoid(),
-      title,
-      content,
-    }
-    dispatch(postAdded(newPost))
+    dispatch(postAdded(title, content))
 
     e.currentTarget.reset()
   }
