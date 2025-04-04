3a096049009fd88f967ef1dda9f923c2f0070798
Paphop Sawasdee <paphop_sawasdee@earthbrain.com>
2025-04-04 15:21:50 +0900

    Add loading spinner

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -1,15 +1,40 @@
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+
 import { useAppDispatch, useAppSelector } from '@/app/hooks'
-import { fetchPosts, selectAllPosts, selectPostsStatus } from './postsSlice'
+
+import { Spinner } from '@/components/Spinner'
+
+import { Post, fetchPosts, selectAllPosts, selectPostsError, selectPostsStatus } from './postsSlice'
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from '@/components/TimeAgo'
 import { ReactionButtons } from './ReactionButtons'
+
+interface PostExcerptProps {
+  post: Post
+}
+
+function PostExcerpt({ post }: PostExcerptProps) {
+  return (
+    <article className="post-excerpt" key={post.id}>
+      <h3>
+        <Link to={`/posts/${post.id}`}>{post.title}</Link>
+      </h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+        <p className="post-content">{post.content.substring(0, 100)}</p>
+        <ReactionButtons post={post} />
+      </div>
+    </article>
+  )
+}
 
 export const PostsList = () => {
   const dispatch = useAppDispatch()
   const posts = useAppSelector(selectAllPosts)
   const postStatus = useAppSelector(selectPostsStatus)
+  const postsError = useAppSelector(selectPostsError)
 
   useEffect(() => {
     if (postStatus === 'idle') {
@@ -17,24 +42,20 @@ export const PostsList = () => {
     }
   }, [postStatus, dispatch])
 
-  const orderedPost = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-
-  const renderedPosts = orderedPost.map((post) => (
-    <article className="post-excerpt" key={post.id}>
-      <h3>
-        <Link to={`/posts/${post.id}`}>{post.title}</Link>
-      </h3>
-      <PostAuthor userId={post.user} />
-      <TimeAgo timestamp={post.date} />
-      <ReactionButtons post={post} />
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-    </article>
-  ))
+  let content: React.ReactNode
+  if (postStatus === 'pending') {
+    content = <Spinner text="loading..." />
+  } else if (postStatus === 'succeeded') {
+    const orderedPost = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+    content = orderedPost.map((post) => <PostExcerpt key={post.id} post={post} />)
+  } else if (postStatus === 'failed') {
+    content = <div>{postsError}</div>
+  }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
