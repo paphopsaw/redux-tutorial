fce887cf775cfd52df9222ebf08dfe1d6cca8297
Paphop Sawasdee <paphop_sawasdee@earthbrain.com>
2025-04-04 19:12:37 +0900

    Add notifications slice

---
+++ src/features/notifications/notificationsSlice.ts
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+import { client } from '@/api/client'
+
+import type { RootState } from '@/app/store'
+import { createAppAsyncThunk } from '@/app/withTypes'
+
+export interface ServerNotification {
+  id: string
+  date: string
+  message: string
+  user: string
+}
+
+export const fetchNotifications = createAppAsyncThunk('notifications/fetchNotifications', async (_unused, thunkApi) => {
+  const allNotifications = selectAllNotifications(thunkApi.getState())
+  const [latestNotification] = allNotifications
+  const latestTimeStamp = latestNotification ? latestNotification.date : ''
+  const response = await client.get<ServerNotification[]>(`/fakeApi/notifications?since=${latestTimeStamp}`)
+  return response.data
+})
+
+const initialState: ServerNotification[] = []
+
+const notificationsSlice = createSlice({
+  name: 'notifications',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchNotifications.fulfilled, (state, action) => {
+      state.push(...action.payload)
+      state.sort((a, b) => b.date.localeCompare(a.date))
+    })
+  },
+})
+
+export default notificationsSlice.reducer
+
+export const selectAllNotifications = (state: RootState) => state.notifications
