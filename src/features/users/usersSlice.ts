364ce87045185c0f4e3395a1ead2f7e8364cd668
Paphop Sawasdee <paphop_sawasdee@earthbrain.com>
2025-04-04 15:38:33 +0900

    Loading users

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,23 +1,32 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { selectCurrentUsername } from '@/features/auth/authSlice'
 
+import { client } from '@/api/client'
+
 import type { RootState } from '@/app/store'
+import { createAppAsyncThunk } from '@/app/withTypes'
 
 interface User {
   id: string
   name: string
 }
 
-const initialState: User[] = [
-  { id: '0', name: 'Tianna Jenkins' },
-  { id: '1', name: 'Kevin Grant' },
-  { id: '2', name: 'Madison Price' },
-]
+export const fetchUsers = createAppAsyncThunk('users/fetchUsers', async () => {
+  const response = await client.get<User[]>('/fakeApi/users')
+  return response.data
+})
+
+const initialState: User[] = []
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload
+    })
+  },
 })
 
 export default usersSlice.reducer
