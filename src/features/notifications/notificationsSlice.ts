2495f806827f41dd00169fffb82b5db3fe764c0d
Paphop Sawasdee <paphop_sawasdee@earthbrain.com>
2025-04-07 17:27:41 +0900

    Normalize Notifications slice

--- src/features/notifications/notificationsSlice.ts
+++ src/features/notifications/notificationsSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createEntityAdapter, createSlice } from '@reduxjs/toolkit'
 
 import { client } from '@/api/client'
 
@@ -25,14 +25,18 @@ export const fetchNotifications = createAppAsyncThunk('notifications/fetchNotifi
   return response.data
 })
 
-const initialState: ClientNotification[] = []
+const notificationsAdapter = createEntityAdapter<ClientNotification>({
+  sortComparer: (a, b) => b.date.localeCompare(a.date),
+})
+
+const initialState = notificationsAdapter.getInitialState()
 
 const notificationsSlice = createSlice({
   name: 'notifications',
   initialState,
   reducers: {
     allNotificationsRead(state) {
-      state.forEach((notification) => {
+      Object.values(state.entities).forEach((notification) => {
         notification.read = true
       })
     },
@@ -45,12 +49,11 @@ const notificationsSlice = createSlice({
         isNew: true,
       }))
 
-      state.forEach((notification) => {
+      Object.values(state.entities).forEach((notification) => {
         notification.isNew = !notification.read
       })
 
-      state.push(...notificationsWithMetadata)
-      state.sort((a, b) => b.date.localeCompare(a.date))
+      notificationsAdapter.upsertMany(state, notificationsWithMetadata)
     })
   },
 })
@@ -59,7 +62,9 @@ export const { allNotificationsRead } = notificationsSlice.actions
 
 export default notificationsSlice.reducer
 
-export const selectAllNotifications = (state: RootState) => state.notifications
+export const { selectAll: selectAllNotifications } = notificationsAdapter.getSelectors(
+  (state: RootState) => state.notifications,
+)
 
 export const selectUnreadNotificationsCount = (state: RootState) => {
   const allNotifications = selectAllNotifications(state)
